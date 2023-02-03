4cc952d828638f69d6a220e46c5f2ab59f9cdcef
aTestProgrammer <phistr@schueler.bwv-ac.de>
2023-02-03 10:13:11 +0100

    1.0 init

---
+++ src/com/roleplay/characters/Character.java
@@ -0,0 +1,11 @@
+package com.roleplay.characters;
+
+import java.awt.geom.Point2D;
+
+public abstract class Character {
+
+    private String name;
+    private double healthPoints;
+    private Point2D position;
+
+}
