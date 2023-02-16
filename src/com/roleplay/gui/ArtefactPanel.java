2f37a21e2596a2b2868e3ac7c14ae961c086a026
klinkenberg_nik <niklas5278.nk@gmail.com>
2023-02-16 09:30:07 +0100

    Add MainFrame, Character Menue, Artefact Menue, Game Settings

---
+++ src/com/roleplay/gui/ArtefactPanel.java
@@ -0,0 +1,17 @@
+package com.roleplay.gui;
+
+import javax.swing.*;
+
+public class ArtefactPanel extends JPanel {
+
+    JPanel artefactPanel;
+
+    ArtefactPanel() {
+
+    }
+
+
+    public JPanel getArtefactPanel() {
+        return artefactPanel;
+    }
+}
---
+++ src/com/roleplay/gui/MainPanel.java
@@ -0,0 +1,16 @@
+package com.roleplay.gui;
+
+import javax.swing.*;
+
+public class MainPanel extends JPanel {
+
+    JPanel mainPanel;
+
+    MainPanel() {
+
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
