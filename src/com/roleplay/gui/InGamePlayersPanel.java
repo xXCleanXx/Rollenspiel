57e2460dfed9928ce9247db519f2f7e61b2f884c
aTestProgrammer <phistr@schueler.bwv-ac.de>
2023-03-09 09:35:01 +0100

    playerPanel

--- src/com/roleplay/gui/InGamePlayersPanel.java
+++ src/com/roleplay/gui/InGamePlayersPanel.java
@@ -2,11 +2,10 @@ package com.roleplay.gui;
 
 import com.roleplay.tiles.characters.Character;
 import com.roleplay.tiles.properties.CharacterProperties;
+import com.roleplay.tools.Image;
 
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicBorders;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class InGamePlayersPanel extends JPanel {
@@ -15,19 +14,8 @@ public class InGamePlayersPanel extends JPanel {
         setLayout(new GridLayout(1,players.size(),10,0));
         setPreferredSize(new Dimension(GameBoard.WIDTH, 60));
 
-        JPanel container = new JPanel(new FlowLayout());
-        playerPanel infos;
-        JLabel texture;
-
         for(Character c : players){
-            infos = new playerPanel(c.getProperties().getName(), ((CharacterProperties) c.getProperties()).getDisplayName(), ((CharacterProperties) c.getProperties()).getHealthPoints(), ((CharacterProperties) c.getProperties()).getMaxHealthPoints());
-            infos.setBorder(new BasicBorders.MarginBorder());
-
-            texture = new JLabel(new ImageIcon(c.getProperties().getTexture()));
-
-            container.add(texture);
-            container.add(infos);
-            add(container);
+            add(new playerPanel(c));
 
         }
     }
@@ -37,18 +25,30 @@ public class InGamePlayersPanel extends JPanel {
         protected JLabel name = new JLabel();
         protected JLabel displayName = new JLabel();
         protected JLabel health = new JLabel();
+        JLabel texture;
+        JPanel info = new JPanel();
 
-        public playerPanel (String name, String displayName, double health, double maxHealth){
+        public playerPanel (Character c) {
+            setLayout(new FlowLayout());
+            this.displayName.setText(((CharacterProperties) c.getProperties()).getDisplayName());
+            this.name.setText(c.getProperties().getName());
+            this.health.setText(((CharacterProperties) c.getProperties()).getHealthPoints() + " / " + ((CharacterProperties) c.getProperties()).getMaxHealthPoints());
 
-            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            texture = new JLabel(new ImageIcon(c.getProperties().getTexture()));
+            info.setLayout(new BoxLayout(info, BoxLayout.Y_AXIS));
+            info.add(this.displayName);
+            info.add(this.name);
+            info.add(this.health);
 
-            this.displayName.setText(displayName);
-            this.name.setText(name);
-            this.health.setText(health + " / " + maxHealth);
+            add(texture);
+            add(info);
+        }
 
-            add(this.displayName);
-            add(this.name);
-            add(this.health);
+        @Override
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            g.drawImage(Image.loadImage("src/com/roleplay/resources/images/buttons/btn_1.png"),0,0,this);
+            Toolkit.getDefaultToolkit().sync();
         }
     }
 
