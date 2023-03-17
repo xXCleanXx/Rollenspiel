ce4c8409d5d9a619006c7bdd6e337e421c22cd3b
nkl <99948687+nkl@users.noreply.github.com>
2023-03-17 11:20:15 +0100

    Change Background for Settings Add Button Listener in ArtefactPanel

--- src/com/roleplay/gui/GameFrame.java
+++ src/com/roleplay/gui/GameFrame.java
@@ -1,24 +1,19 @@
 package com.roleplay.gui;
 
 import com.roleplay.characters.Character;
-import com.roleplay.map.GameMap;
-import com.roleplay.map.GameMapCreator;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class GameFrame extends JFrame {
 
-    public GameFrame(ArrayList<Character> player) {
-
     private GameBoard board;
 
-    public GameFrame(ArrayList<Character> player){
+    public GameFrame(ArrayList<Character> player) {
         super();
         initialize(player);
     }
@@ -30,8 +25,9 @@ public class GameFrame extends JFrame {
         controlBar.add(new JMenu("Settings"));
         setJMenuBar(controlBar);
 
-         board = new GameBoard(player);
-        add(board, BorderLayout.CENTER);addKeyBindings(board);
+        board = new GameBoard(player);
+        add(board, BorderLayout.CENTER);
+        addKeyBindings(board);
 
         this.addKeyListener(new KeyAdapter() {
             @Override
@@ -43,8 +39,9 @@ public class GameFrame extends JFrame {
 
         InGamePlayersPanel players = new InGamePlayersPanel(player);
         add(players, BorderLayout.WEST);
-ControlPanel gameControl = new ControlPanel();
-            add(gameControl, BorderLayout.EAST);        revalidate();
+        ControlPanel gameControl = new ControlPanel();
+        add(gameControl, BorderLayout.EAST);
+        revalidate();
         repaint();
 
         setResizable(false);
@@ -54,19 +51,15 @@ ControlPanel gameControl = new ControlPanel();
         setLocationRelativeTo(null);
         setVisible(true);
     }
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setLocationRelativeTo(null);
-            setVisible(true);
-        }
 
     public void addKeyBindings(JComponent jc) {
-        setKeyActions("RIGHT", KeyEvent.VK_RIGHT,1,0, jc);
-        setKeyActions("LEFT", KeyEvent.VK_LEFT,-1,0, jc);
-        setKeyActions("UP", KeyEvent.VK_UP,0,-1,jc);
-        setKeyActions("DOWN", KeyEvent.VK_DOWN,0,1, jc);
+        setKeyActions("RIGHT", KeyEvent.VK_RIGHT, 1, 0, jc);
+        setKeyActions("LEFT", KeyEvent.VK_LEFT, -1, 0, jc);
+        setKeyActions("UP", KeyEvent.VK_UP, 0, -1, jc);
+        setKeyActions("DOWN", KeyEvent.VK_DOWN, 0, 1, jc);
     }
 
-    private void setKeyActions(String keyName, int key, int dx, int dy, JComponent jc){
+    private void setKeyActions(String keyName, int key, int dx, int dy, JComponent jc) {
         jc.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, 0, false), keyName);
         jc.getActionMap().put(keyName, new AbstractAction() {
             @Override
@@ -74,11 +67,11 @@ ControlPanel gameControl = new ControlPanel();
 
                 for (Character c : MainFrame.getCharacterList()) {
                     if (c.getProperties().isMyTurn()) {
-                        if(!board.getGameMap().getMapElements()[c.getProperties().getPosition().y + dy][c.getProperties().getPosition().x + dx].getMapElementProperties().getHitBox().isEnabled()){
-                            c.getProperties().getPosition().translate(dx,dy);
-                            ControlPanel.setValue(ControlPanel.getValue() -1);
+                        if (!board.getGameMap().getMapElements()[c.getProperties().getPosition().y + dy][c.getProperties().getPosition().x + dx].getMapElementProperties().getHitBox().isEnabled()) {
+                            c.getProperties().getPosition().translate(dx, dy);
+                            ControlPanel.setValue(ControlPanel.getValue() - 1);
                             ControlPanel.showValue.setText(String.valueOf(ControlPanel.getValue()));
-                            if(ControlPanel.getValue() == 0) {
+                            if (ControlPanel.getValue() == 0) {
                                 c.getProperties().setMyTurn(false);
                                 ControlPanel.leftInRound.remove(c);
                                 ControlPanel.button.setEnabled(true);
@@ -89,8 +82,5 @@ ControlPanel gameControl = new ControlPanel();
                 }
             }
         });
-
     }
-
-
 }
