5fe194f3cd13c30d1c58bb6098d8f582328e8aee
nkl <99948687+nkl@users.noreply.github.com>
2023-03-24 16:51:54 +0100

    Charachter and Item Create FightPanel

--- src/com/roleplay/items/weapons/HandAxe.java
+++ src/com/roleplay/items/weapons/HandAxe.java
@@ -2,9 +2,12 @@ package com.roleplay.items.weapons;
 
 
 import com.roleplay.items.ItemProperties;
+import com.roleplay.tools.ImageUtils;
 
 public class HandAxe extends Weapon {
-    public HandAxe(ItemProperties itemProperties, double damage) {
-        super(itemProperties, damage);
+    public HandAxe(ItemProperties itemProperties) {
+        super(itemProperties, 6);
+        itemProperties.setName("handAxe");
+        itemProperties.setTexture32(ImageUtils.loadImage("src/com/roleplay/resources/images/items/hand_axe_32x32.png"));
     }
 }
